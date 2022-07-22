c76e8a3698bc60547f2926e1721b110a440a1fb3
Lian <alejovaca18@gmail.com>
2022-07-22 13:15:08 -0500

    Seccion 4 del curos

---
+++ src/app/heros/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+
+
+
+@Component({
+    selector: 'app-hero',
+    templateUrl: 'hero.component.html'
+})
+export class HeroComponent{
+    nombre: string = 'Ironman';
+    edad: number = 45;
+
+    get nombreCapitalizado():string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarNombre():void{
+        this.nombre = 'SpiderMan';
+    }
+
+    cambiarEdad():void{
+        this.edad = 26;
+    }
+}
